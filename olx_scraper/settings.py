2e1eab5cfea252c24ecb42629143b217e069c3b6
Господин <sumsunh777@gmail.com>
2021-06-01 14:12:22 +0300

    user-agent change

--- olx_scraper/settings.py
+++ olx_scraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'olx_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'olx_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
